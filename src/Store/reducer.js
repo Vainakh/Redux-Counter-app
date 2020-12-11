b174406b55e9e3ec24b983ecb65c857ff6ff40e8
Adlan Yandarbiev <adlanyandarbiev@gmail.com>
2020-12-11 15:43:40 -0800

    Passing and retrieving data with actions

--- src/Store/reducer.js
+++ src/Store/reducer.js
@@ -8,6 +8,25 @@ const reducer = (state = initialState, action) => {
       counter: state.counter + 1
     }
   }
+
+  if (action.type === 'DECREMENT') {
+    return {
+      counter: state.counter - 1
+    }
+  }
+
+  if (action.type === 'ADD5') {
+    return {
+      counter: state.counter + action.val
+    }
+  }
+
+  if (action.type === 'SUBSTRUCT5') {
+    return {
+      counter: state.counter - action.val
+    }
+  }
+
   return state;
 };
 
--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -31,9 +31,9 @@ class Counter extends Component {
             <div>
                 <CounterOutput value={this.props.ctr} />
                 <CounterControl label="Increment" clicked={ this.props.onIncrementCounter } />
-                <CounterControl label="Decrement" clicked={() => this.counterChangedHandler( 'dec' )}  />
-                <CounterControl label="Add 5" clicked={() => this.counterChangedHandler( 'add', 5 )}  />
-                <CounterControl label="Subtract 5" clicked={() => this.counterChangedHandler( 'sub', 5 )}  />
+                <CounterControl label="Decrement" clicked={ this.props.onDecrementCounter }  />
+                <CounterControl label="Add 5" clicked={ this.props.onIncrementFiveCounter } />
+                <CounterControl label="Subtract 5" clicked={ this.props.onDecrementFiveCounter }  />
             </div>
         );
     }
@@ -47,7 +47,10 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
     return {
-        onIncrementCounter: () => dispatch({ type: 'INCREMENT' })
+        onIncrementCounter: () => dispatch({ type: 'INCREMENT' }),
+        onDecrementCounter: () => dispatch({ type: 'DECREMENT' }),
+        onIncrementFiveCounter: () => dispatch({ type: 'ADD5', val: 5 }),
+        onDecrementFiveCounter: () => dispatch({ type: 'SUBSTRUCT5', val: 5 }),
     };
 };
 
