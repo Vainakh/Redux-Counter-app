70db6a560efcff5f4551f5505be8f2c599d59694
Adlan Yandarbiev <adlanyandarbiev@gmail.com>
2020-12-11 16:00:41 -0800

    Use of switch case in reducer

--- src/Store/reducer.js
+++ src/Store/reducer.js
@@ -3,29 +3,48 @@ const initialState = {
 };
 
 const reducer = (state = initialState, action) => {
-  if (action.type === 'INCREMENT') {
-    return {
-      counter: state.counter + 1
-    }
+  switch (action.type){
+    case 'INCREMENT':
+      return {
+            counter: state.counter + 1
+          }
+    case 'DECREMENT':
+      return {
+            counter: state.counter - 1
+          }
+    case 'ADD5':
+      return {
+            counter: state.counter + action.val
+          }
+    case 'SUBSTRUCT5':
+      return {
+            counter: state.counter - action.val
+          }
   }
+    
+  // if (action.type === 'INCREMENT') {
+  //   return {
+  //     counter: state.counter + 1
+  //   }
+  // }
 
-  if (action.type === 'DECREMENT') {
-    return {
-      counter: state.counter - 1
-    }
-  }
+  // if (action.type === 'DECREMENT') {
+  //   return {
+  //     counter: state.counter - 1
+  //   }
+  // }
 
-  if (action.type === 'ADD5') {
-    return {
-      counter: state.counter + action.val
-    }
-  }
+  // if (action.type === 'ADD5') {
+  //   return {
+  //     counter: state.counter + action.val
+  //   }
+  // }
 
-  if (action.type === 'SUBSTRUCT5') {
-    return {
-      counter: state.counter - action.val
-    }
-  }
+  // if (action.type === 'SUBSTRUCT5') {
+  //   return {
+  //     counter: state.counter - action.val
+  //   }
+  // }
 
   return state;
 };
